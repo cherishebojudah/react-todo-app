0f6a9aa8e2074809ba05b3a56a64a623fb205203
Cherish Collins <cherish.ebojudah@gmail.com>
2021-07-21 23:40:54 +0100

    create todo component

--- src/App.js
+++ src/App.js
@@ -1,9 +1,11 @@
 import React from 'react';
+import TodoApp from './Todo';
 
 function App() {
   return (
     <div className="App">
      <h1>Hello to you!</h1>
+     <TodoApp/>
     </div>
   );
 }
