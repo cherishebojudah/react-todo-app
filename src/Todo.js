11ec867bde448bbb620c0ac53dfae0974ce2b81e
Cherish Collins <cherish.ebojudah@gmail.com>
2021-07-22 00:13:12 +0100

    add handlechange function for input field

--- src/Todo.js
+++ src/Todo.js
@@ -1,13 +1,20 @@
-import React from 'react';
+import React, { useState } from 'react';
 
 function TodoApp () {
+    const [state, setState] = useState({items: [], text: ''});
+
+    const handleChange = (e) => {
+        setState({text: e.target.value});
+    }
+
+
     return(
         <div>
             <h3>TODO</h3>
             <p>Hey, let's make a todo list!</p>
             <form >
                 <label htmlFor='new-todo'>What needs to be done?</label>
-                <input id='new-todo'/>
+                <input id='new-todo' onChange={handleChange} value={state.text} />
             </form>
             <button>Add Number</button>
         </div>
