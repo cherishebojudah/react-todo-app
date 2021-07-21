b8bc03c2f83a2086d15a1cbe9009f861fa0c0467
Cherish Collins <cherish.ebojudah@gmail.com>
2021-07-21 23:50:06 +0100

    add button element to todo component

--- src/Todo.js
+++ src/Todo.js
@@ -9,6 +9,7 @@ function TodoApp () {
                 <label htmlFor='new-todo'>What needs to be done?</label>
                 <input id='new-todo'/>
             </form>
+            <button>Add Number</button>
         </div>
     )
 }
